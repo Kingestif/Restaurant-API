4d5d2ca7eed52091b362d3c74eda9fa8433f3963
Kingestif <kingestiff@gmail.com>
2025-07-10 16:45:18 +0300

    fix: correct category enum values in menuValidation to ensure consistency with prisma schema

--- validation/menuValidation.ts
+++ validation/menuValidation.ts
@@ -4,7 +4,7 @@ export const menuValidation = z.object({
     name: z.string().min(1, 'name is required'),
     description: z.string().min(1, 'description is required'),
     price: z.number().min(1, 'number is required'),
-    category: z.enum(['Appetizer', 'Main Course', 'Dessert', 'Drink'], {
+    category: z.enum(['Appetizer', 'MainCourse', 'Dessert', 'Drink'], {
         message: 'category must be one of Appetizer, Main Course, Dessert, or Drink'
     }),
     available: z.boolean().default(true)
