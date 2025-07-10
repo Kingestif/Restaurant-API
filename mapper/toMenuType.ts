7901c2fd120bc01729e7e95e68b2da86b7aeacb1
Kingestif <kingestiff@gmail.com>
2025-07-10 16:43:58 +0300

    fix: update toMenu function to handle both mogogdb  _id and prisma id

--- mapper/toMenuType.ts
+++ mapper/toMenuType.ts
@@ -2,7 +2,7 @@ import { MenuType } from "../types/menu"
 
 export const toMenu = (doc:any):MenuType => {
     return {
-        id: doc._id.toString(),
+        id: (doc._id ?? doc.id)?.toString(),
         name: doc.name,
         description: doc.description,
         price: doc.price,
