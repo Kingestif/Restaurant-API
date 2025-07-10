f4af47531d2aa4457ae98b25f5be8966c50d804c
Kingestif <kingestiff@gmail.com>
2025-07-10 09:15:24 +0300

    fix: allow mapper to handle both mongoDB _id and postgres id fields

--- mapper/toUserType.ts
+++ mapper/toUserType.ts
@@ -2,7 +2,7 @@ import { Usertype } from "../types/user";
 
 export const toUser = (doc: any): Usertype => {
     return {
-        id: doc._id.toString(),
+        id: (doc.id ?? doc._id)?.toString(),
         email: doc.email,
         role: doc.role
     }
